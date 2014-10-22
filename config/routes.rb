1e585fa5fd08d8514aa53cf3cf367b12c4168803
Ansar Tokbergenov <suric05@gmail.com>
2014-10-22 14:08:05 +0000

    add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   match "/contact", to: 'static_pages#contact', via: :get
   
   match "/help", to: 'static_pages#help', via: :get
+  
+  match "/home", to: 'static_pages#home', via: :get
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
