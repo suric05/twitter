62b180d1860af9a686ca6178ae2d2f16dbf107a4
Ansar Tokbergenov <suric05@gmail.com>
2014-10-22 13:55:52 +0000

    add static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def full_title(page_title)
+    if page_title.empty?
+      return "Twitter"
+    else
+      return "#{page_title}" | "Twitter"
+    end
+  end
+  
 end
